46ad6d61b6ac191c30d88e5e8116fa4924c65254
LucasVith <lucasvths@gmail.com>
2026-09-09 20:43:36 -0300

    calculos

--- main.c
+++ main.c
@@ -5,8 +5,14 @@ int main(){
     int a = 5;
     int b = 4;
 
-    printf("o maior número é: %d\n", (a > b) ? a : b);
 
+    printf("o valor da multiplicação é: %d\n",(a * b));
+
+    printf("o valor da divisão é: %d\n", (a / b));
+
+    printf ("o valor da subtração é: %d\n", (a - b));
+
+    printf ("o valor da soma é: %d\n", (a + b));
 
 
   return 0;
