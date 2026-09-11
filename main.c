73a0986a6df461c5c53c2402afaf21bde5500207
LucasVith <lucasvths@gmail.com>
2026-09-11 19:38:23 -0300

    antecessor e sucessor

--- main.c
+++ main.c
@@ -1,26 +1,22 @@
 #include<stdio.h>
 #include<locale.h>
-#include<stdbool.h>
-bool hepar(int numero){
-    return(numero % 2 == 0);
-}
+
+
 
 int main(){
 
    setlocale(LC_ALL, "pt_BR.UTF-8");
 
-   bool resultado1 = hepar(4);
-   bool resultado2 = hepar(7);
-   bool resultado3 = hepar(20);
-   bool resultado4 = hepar(9);
-   bool resultado5 = hepar(11);
+   int n;
+   printf("digite um número: ");
+   scanf("%d", &n);
 
-   printf("4 é par? %d\n", resultado1);
-   printf("7 é par? %d\n", resultado2);
-   printf("20 é par? %d\n", resultado3);
-   printf("9 é par? %d\n", resultado4);
-   printf("11 é par? %d\n", resultado5);
+   printf ("antecessor: %d\n", n-1);
+   printf ("sucessor: %d\n",n+1);
 
+
+ 
+   
  
   return 0;
 }
