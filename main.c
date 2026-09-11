ed7443813f53db5c954d95e0888523475950444d
LucasVith <lucasvths@gmail.com>
2026-09-11 20:24:36 -0300

    cotação do dolar

--- main.c
+++ main.c
@@ -7,14 +7,14 @@ int main(){
 
    setlocale(LC_ALL, "pt_BR.UTF-8");
 
-  int idade;
+  float valor, cotacao;
 
-  printf("digite sua idade:");
-  scanf("%d",&idade);
-
-  printf("equivale a %d meses\n", idade * 12);
-  printf("equivale a %d dias (aproximado)\n", idade * 365);
- 
+  printf("digite o valor em reais:");
+  scanf("%f", &valor);
+  printf("digite a cotação do dolar:");
+  scanf("%f", &cotacao);
+  
+  printf("valor em dolares:%.2f\n", valor / cotacao);
    
  
   return 0;
