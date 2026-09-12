1b8f7b64c466d5a8631b9324a8d2c6083f57e8f3
LucasVith <lucasvths@gmail.com>
2026-09-11 21:33:46 -0300

    celsius em fahrenheit

--- main.c
+++ main.c
@@ -7,14 +7,13 @@ int main(){
 
    setlocale(LC_ALL, "pt_BR.UTF-8");
 
-  float n1, n2, n3, media;
+float celsius, fahrenheit;
 
-  printf("digite três notas:");
-  scanf("%f %f %f",&n1, &n2, &n3);
+printf("digite a temperatura em celsius:");
+scanf("%f", &celsius);
 
-  media = (n1 + n2 + n3) / 3;
-  printf("média: %.2f\n", media);
-
+fahrenheit = (celsius * 9.0 / 5.0) + 32;
+printf("fahrenheit: %.2f\n", fahrenheit);
    
  
   return 0;
