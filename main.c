d74ea753f3ada07b30c9ea206b5f12b893842aa7
LucasVith <lucasvths@gmail.com>
2026-09-11 21:41:01 -0300

    segundos, minutos e horas

--- main.c
+++ main.c
@@ -7,13 +7,15 @@ int main(){
 
    setlocale(LC_ALL, "pt_BR.UTF-8");
 
-float celsius, fahrenheit;
+int segundos, horas, minutos, resto;
+printf("digite a quantidade de segundos:");
+scanf("%d", &segundos);
 
-printf("digite a temperatura em celsius:");
-scanf("%f", &celsius);
+horas = segundos / 3600;
+minutos = (segundos % 3600) / 60;
+resto = segundos % 60;
 
-fahrenheit = (celsius * 9.0 / 5.0) + 32;
-printf("fahrenheit: %.2f\n", fahrenheit);
+printf ("%d horas, %d minutos e %d segundos\n", horas, minutos, resto);
    
  
   return 0;
