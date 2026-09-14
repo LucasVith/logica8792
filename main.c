03c8c0a32a269d89cc33279d8d4770b35ffb1be8
LucasVith <lucasvths@gmail.com>
2026-09-14 20:19:47 -0300

    maior numero

--- main.c
+++ main.c
@@ -7,22 +7,15 @@ int main(){
 
    setlocale(LC_ALL, "pt_BR.UTF-8");
 
-int idade;
+int a, b, c, maior;
+printf("digite três números:");
+scanf("%d %d %d", &a, &b, &c);
 
-printf("digite uma idade: ");
-scanf("%d", &idade);
-if(idade >= 50){
-   printf("você é senior!");
-}else if(idade >= 18){
-printf("você é maior de idade");
-}else if(idade >= 12){
-printf("você é adolescente");
-}else if(idade >=9){
-   printf("você é pré-adolescente");
-}else{
-   printf(" você é criança!");
-}
-   
+maior = a;
+if(b > maior) maior = b;
+if(c > maior) maior = c;
+
+   printf(" maior: %d", maior);
  
   return 0;
 }
