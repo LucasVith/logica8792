3dffcf58cb23331bf73eae6d0df5197ddba2f936
LucasVith <lucasvths@gmail.com>
2026-09-16 19:40:58 -0300

    if else || 2

--- main.c
+++ main.c
@@ -5,20 +5,20 @@ int main(){
 
    setlocale(LC_ALL, "pt_BR.UTF-8");
 
- int idade;
- float altura;
+int idade;
+int ehestudante;
 
-printf("bem-vindos ao brinquedo roleta da morte!");
-printf("\nqual sua idade?");
+printf("qual a sua idade?");
 scanf("%d", &idade);
-printf("\nqual sua altura?");
-scanf("%f", &altura);
+printf("\nvocê é estudante (0 - não | 1 - sim:)");
+scanf("%d", &ehestudante);
 
-if((idade >= 18) || (altura >= 1.60)){
-  printf("você pode brincar na roleta!");
+if((idade >=60)&& (ehestudante ==1)){
+  printf("parabens, você tem direito a desconto!");
 }else{
-  printf("você não pode brincar na roleta!");
+  printf("infelizmente você não tem desconto");
 }
 
-  return 0;
+
+return 0;
 }
