9757b3af0c2138ff21ac4dc7d7a93935ea2550c3
LucasVith <lucasvths@gmail.com>
2026-09-16 20:53:37 -0300

    switch

--- main.c
+++ main.c
@@ -5,26 +5,37 @@ int main(){
 
    setlocale(LC_ALL, "pt_BR.UTF-8");
 
-int diadasemana;
+int dia;
+printf("digite um número de 1 a 7:");
+scanf("%d", &dia);
 
-printf("digite um número de (1 a 7): ");
-scanf("%d", &diadasemana);
-if(diadasemana ==1){
-  printf("domingo");
-}else if(diadasemana == 2){
-  printf("segunda-feira");
-}else if(diadasemana == 3){
-  printf("terça-feira");
-}else if(diadasemana == 4){
-  printf("quarta-feira");
-}else if(diadasemana == 5){
-  printf("quinta-feira");
-}else if(diadasemana == 6){
-  printf("sexat-feira");
-}else if(diadasemana == 7){
-  printf("sábado");
-}else{
-  printf("valor invalido!");
+switch(dia){
+  case 1:
+   printf("domingo");
+   break;
+  case 2:
+   printf("segunda-feira");
+   break;
+  case 3:
+   printf("terça-feira");
+   break;
+  case 4:
+   printf("quarta-feira");
+   break;
+  case 5:
+   printf("quinta-feira");
+   break;
+  case 6:
+   printf("sexta-feira");
+   break;
+  case 7:
+   printf("sabado");
+   break;
+  default:
+   printf("número ivalido!!");
+   break;
+
+
 }
 
 
