04a633459281ef203f174d7951de7aad87e4658c
LucasVith <lucasvths@gmail.com>
2026-09-16 21:44:32 -0300

    lados de um triangulo

--- main.c
+++ main.c
@@ -5,27 +5,18 @@ int main(){
 
    setlocale(LC_ALL, "pt_BR.UTF-8");
 
-float n1, n2, n3 , media;
+float a, b, c;
 
-printf("digite a nota 1:");
-scanf("%f", &n1);
+printf("digite os três lados do triângulo:");
+scanf("%f %f %f", &a, &b, &c);
 
-printf("digite a nota 2:");
-scanf("%f", &n2);
-
-printf("digite a nota 3:");
-scanf("%f", &n3);
- media= (n1 +n2 + n3) / 3;
-
- if(media >=7){
-  printf("aprovado!");
- }else if(media >=5){
-  printf("recuperação");
- }else{
- printf("reprovado!");
- }
-
-
+if(a == b && b ==c){
+  printf("equilatéro!\n");
+}else if(a ==b || a == c || b ==c){
+  printf("isosceles!\n");
+}else{
+  printf("escaleno!\n");
+}
 
 return 0;
 }
