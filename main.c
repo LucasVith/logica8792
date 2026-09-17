2285a0f74a14bcfa1983b9d67cde8fc953fc1ac1
LucasVith <lucasvths@gmail.com>
2026-09-16 21:26:51 -0300

    if else else if

--- main.c
+++ main.c
@@ -5,38 +5,25 @@ int main(){
 
    setlocale(LC_ALL, "pt_BR.UTF-8");
 
-int dia;
-printf("digite um número de 1 a 7:");
-scanf("%d", &dia);
+float n1, n2, n3 , media;
 
-switch(dia){
-  case 1:
-   printf("domingo");
-   break;
-  case 2:
-   printf("segunda-feira");
-   break;
-  case 3:
-   printf("terça-feira");
-   break;
-  case 4:
-   printf("quarta-feira");
-   break;
-  case 5:
-   printf("quinta-feira");
-   break;
-  case 6:
-   printf("sexta-feira");
-   break;
-  case 7:
-   printf("sabado");
-   break;
-  default:
-   printf("número ivalido!!");
-   break;
+printf("digite a nota 1:");
+scanf("%f", &n1);
 
+printf("digite a nota 2:");
+scanf("%f", &n2);
 
-}
+printf("digite a nota 3:");
+scanf("%f", &n3);
+ media= (n1 +n2 + n3) / 3;
+
+ if(media >=7){
+  printf("aprovado!");
+ }else if(media >=5){
+  printf("recuperação");
+ }else{
+ printf("reprovado!");
+ }
 
 
 
