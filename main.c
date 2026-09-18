01841a94f81bb2ce0d295f6d40b324f6a8f1f75f
LucasVith <lucasvths@gmail.com>
2026-09-18 20:14:11 -0300

    switch estações

--- main.c
+++ main.c
@@ -5,34 +5,44 @@ int main(){
 
    setlocale(LC_ALL, "pt_BR.UTF-8");
 
-int opcao;
+int mes;
 
-printf("_____ sistema de pedágio---\n");
-printf("1 - moto\n");
-printf("2 - carro passeio\n");
-printf("3- caminhão\n");
-printf("4 - ônibus\n");
-scanf("%d", &opcao);
+printf("digite um número de 1 a 12:");
+scanf("%d", &mes);
 
-switch (opcao)
-{
+switch (mes){
 
-case 1:
-  printf("categoria: moto | tarifa: R$ 5,00\n");
+  case 1:
+  case 2:
+  case 12:
+  printf("verão");
   break;
-case 2:
-  printf("categoria: carro passeio | tarifa: R$ 10,00\n");
+
+  case 3:
+  case 4:
+  case 5:
+  printf("outono");
   break;
-case 3:
-  printf("categoria: caminhão | tarifa: R$ 25,00\n");
+
+  case 6 :
+  case 7 :
+  case 8 :
+  printf("inverno");
   break;
-case 4:
-  printf("categiria: ônibus | tarifa: R$ 30,00\n");
+ 
+  case 9 :
+  case 10 :
+  case 11:
+  printf("primavera");
   break;
-default:
-  printf("erro: categoria não cadastrada no sistema.\n");
+  
+  default:
+  printf("número invalido!");
   break;
+
+  return 0;
 
 }
+
 
 }
