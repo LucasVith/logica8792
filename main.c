8a9f926e3638513b5a17cda16875835a4ca4f9d9
LucasVith <lucasvths@gmail.com>
2026-09-18 19:18:46 -0300

    switch sistema de pedágio

--- main.c
+++ main.c
@@ -5,15 +5,34 @@ int main(){
 
    setlocale(LC_ALL, "pt_BR.UTF-8");
 
-float a, b, c;
+int opcao;
 
-printf("digite os três lados do triângulo:");
-scanf("%f %f %f", &a, &b, &c);
+printf("_____ sistema de pedágio---\n");
+printf("1 - moto\n");
+printf("2 - carro passeio\n");
+printf("3- caminhão\n");
+printf("4 - ônibus\n");
+scanf("%d", &opcao);
 
-if(a+b > c && a +c > b && b +c >a){
-  printf("os lados formam um triângulo!\n");
-}else{
-  printf("os lados não formam um triângulo!\n");
+switch (opcao)
+{
+
+case 1:
+  printf("categoria: moto | tarifa: R$ 5,00\n");
+  break;
+case 2:
+  printf("categoria: carro passeio | tarifa: R$ 10,00\n");
+  break;
+case 3:
+  printf("categoria: caminhão | tarifa: R$ 25,00\n");
+  break;
+case 4:
+  printf("categiria: ônibus | tarifa: R$ 30,00\n");
+  break;
+default:
+  printf("erro: categoria não cadastrada no sistema.\n");
+  break;
+
 }
-return 0;
+
 }
