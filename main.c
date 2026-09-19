7f16ead63019e282ab2434604802e94b343022ec
LucasVith <lucasvths@gmail.com>
2026-09-18 21:41:11 -0300

    imc

--- main.c
+++ main.c
@@ -5,21 +5,25 @@ int main(){
 
    setlocale(LC_ALL, "pt_BR.UTF-8");
 
-int ano;
-printf("digite o ano: ");
-scanf("%d", &ano);
+float peso, altura, imc;
 
-if((ano % 4 == 0 && ano % 100 !=0) || ano % 400 ==0){
-  printf("ano: %d\n", ano);
-  printf("ano bissexto!\n");
+printf("dogote o peso (kg): ");
+scanf("%f", &peso);
+
+printf("digite sua altura (m): ");
+scanf("%f", &altura);
+
+imc = peso / (altura * altura);
+
+if(imc < 18.5){
+  printf("Classificação: abaixo do peso!\n");
+}else if(imc < 25){
+  printf("Classificção: peso normal!\n");
+}else if(imc < 30){
+  printf("Classificação: sobrepeso!\n");
 }else{
-  printf("ano: %d\n", ano);
-  printf("ano não bissexto!\n");
+  printf("classificação:obesidade!\n");
 }
-
-
-
-  
 
   return 0;
 
