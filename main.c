6bd05bbe66c4f3a2c5ea87c1f68714276d5f949b
LucasVith <lucasvths@gmail.com>
2026-09-21 19:45:01 -0300

    comando for

--- main.c
+++ main.c
@@ -5,26 +5,10 @@ int main(){
 
    setlocale(LC_ALL, "pt_BR.UTF-8");
 
-float peso, altura, imc;
-
-printf("dogote o peso (kg): ");
-scanf("%f", &peso);
-
-printf("digite sua altura (m): ");
-scanf("%f", &altura);
-
-imc = peso / (altura * altura);
-
-if(imc < 18.5){
-  printf("Classificação: abaixo do peso!\n");
-}else if(imc < 25){
-  printf("Classificção: peso normal!\n");
-}else if(imc < 30){
-  printf("Classificação: sobrepeso!\n");
-}else{
-  printf("classificação:obesidade!\n");
+for(int i = 0; i < 10000; i++){
+  printf("%d\n", i);
 }
-
+printf("\nMeu for funcionou!!");
   return 0;
 
 
