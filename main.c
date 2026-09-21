33356723761ea80af7470057f600f2f1f951877c
LucasVith <lucasvths@gmail.com>
2026-09-21 19:49:55 -0300

    for com texto

--- main.c
+++ main.c
@@ -5,8 +5,8 @@ int main(){
 
    setlocale(LC_ALL, "pt_BR.UTF-8");
 
-for(int i = 0; i < 10000; i++){
-  printf("%d\n", i);
+for(int i = 0; i < 10; i++){
+  printf("%s\n %d", "\nOS números estão aumentando: ------>" ,i);
 }
 printf("\nMeu for funcionou!!");
   return 0;
