a0c0e33737697c5054f685537b750f7d4f1b0c22
LucasVith <lucasvths@gmail.com>
2026-09-21 20:07:16 -0300

    char nomes

--- main.c
+++ main.c
@@ -5,10 +5,12 @@ int main(){
 
    setlocale(LC_ALL, "pt_BR.UTF-8");
 
-for(int i = 0; i < 10; i++){
-  printf("%s\n %d", "\nOS números estão aumentando: ------>" ,i);
-}
-printf("\nMeu for funcionou!!");
+char nomes [4][20] = {"lucas", "diego", "gustavo", "marcos"};
+
+printf("%s\n", nomes[0]);
+printf("%s\n", nomes[1]);
+printf("%s\n", nomes[2]);
+printf("%s\n", nomes[3]);
   return 0;
 
 
