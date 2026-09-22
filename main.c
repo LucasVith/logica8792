541281cadf3d6c965125bdcdd67c4011a6629974
LucasVith <lucasvths@gmail.com>
2026-09-21 21:03:46 -0300

    array nome e idade

--- main.c
+++ main.c
@@ -5,17 +5,10 @@ int main(){
 
    setlocale(LC_ALL, "pt_BR.UTF-8");
 
-int numerosInteiros[5] = {1, 5, 10, 15, 20};
-float numerosFloats[5] = {10.5, 20.8, 30.2, 40.9, 50.1};
-char carros[3][20] = {"ferrari", "mercedez", "mazda"};
-
-printf("%d %d %d %d %d\n", numerosInteiros[0],numerosInteiros[1],numerosInteiros[2],numerosInteiros[3],numerosInteiros[4]);
-
-printf("%.1f %.1f %.1f %.1f %.1f\n", numerosFloats[0],numerosFloats[1],numerosFloats[2],numerosFloats[3],numerosFloats[4]);
-
-printf("%s %s %s\n", carros[0],carros[1],carros[2]);
-
-
+int idades[3] = {25,35,45};
+printf("%s %d %s\n", "Manoel tem, ", idades[0], "anos!");
+printf("%s %d %s\n", "Lucas tem,", idades[1], "anos!");
+printf("%s %d %s\n", "Gabriel tem, ", idades[2], " anos!");
 return 0;
 
 
