01bcd9d23cdc15e45e0719dc8ca2e2faa38e3fd0
LucasVith <lucasvths@gmail.com>
2026-09-22 19:32:33 -0300

    funções

--- main.c
+++ main.c
@@ -1,18 +1,17 @@
 #include<stdio.h>
 #include<locale.h>
 
-void dobrar(int *x){
-   *x = *x * 2;
+void saudar(){
+   printf("Olá mundo!\n");
+   printf("salve salve");
 }
 
 int main(){
 
    setlocale(LC_ALL, "pt_BR.UTF-8");
 
-int numero = 10;
- dobrar(&numero);
- printf("Número depois da função %d\n", numero);
-// teste
+saudar();
+
 
 
 return 0;
