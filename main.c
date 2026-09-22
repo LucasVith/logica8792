c13843d9b689da3417b833018774b225c1346ca4
LucasVith <lucasvths@gmail.com>
2026-09-22 19:26:30 -0300

    função

--- main.c
+++ main.c
@@ -1,19 +1,18 @@
 #include<stdio.h>
 #include<locale.h>
 
+void dobrar(int *x){
+   *x = *x * 2;
+}
+
 int main(){
 
    setlocale(LC_ALL, "pt_BR.UTF-8");
 
-
-   int x = 10;
-   int *p = &x;
-
-   printf("valor de x : %d\n", x);
-   printf("Endedereço de x: %p\n", &x);
-   printf("Valor via ponteiro: %d\n", *p);
-
-
+int numero = 10;
+ dobrar(&numero);
+ printf("Número depois da função %d\n", numero);
+// teste
 
 
 return 0;
