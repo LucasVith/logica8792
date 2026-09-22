e495585a7dc02cf49a182644aece9e2660c50979
LucasVith <lucasvths@gmail.com>
2026-09-22 19:41:13 -0300

    função saudar

--- main.c
+++ main.c
@@ -1,16 +1,18 @@
 #include<stdio.h>
 #include<locale.h>
 
-void saudar(){
-   printf("Olá mundo!\n");
-   printf("salve salve");
+void saudar(const char *nome){
+   printf("Olá %s\n", nome);
+   
 }
 
 int main(){
 
    setlocale(LC_ALL, "pt_BR.UTF-8");
 
-saudar();
+saudar("João");
+saudar("Lucas");
+saudar("Vith");
 
 
 
