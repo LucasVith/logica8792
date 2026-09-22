d60c409d02eebe6f035c90d05ba153b9709ea9ef
LucasVith <lucasvths@gmail.com>
2026-09-21 21:43:46 -0300

    troca de valor array

--- main.c
+++ main.c
@@ -4,15 +4,11 @@
 int main(){
 
    setlocale(LC_ALL, "pt_BR.UTF-8");
-int soma = 0;
-int i;
-for( i = 1; i <=5; i ++){
-  
-  soma = soma + i;
-  
-  
-}
-printf(" soma é %d", soma);
+int numeros[] = {1, 2, 3};
+
+numeros [0] = 10;
+
+printf("%d", numeros[0]);
 
 
 return 0;
