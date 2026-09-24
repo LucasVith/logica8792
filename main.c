fa0c5a790d1701be9f9b098fb828b16d2c2b68c1
LucasVith <lucasvths@gmail.com>
2026-09-24 20:16:48 -0300

    while soma

--- main.c
+++ main.c
@@ -8,29 +8,20 @@ int main(){
 setlocale(LC_ALL, "pt_BR.UTF-8");
 
  
- int opcao = -1;
- while(opcao != 0){
-  printf("\nmenu: \n");
-  printf("1 - depositar\n");
-  printf("2 - sacar\n");
-  printf("3 - consultar\n");
-  printf("0 - sair\n");
-  printf("escolha: ");
-  scanf("%d", &opcao);
- 
-switch(opcao){
-  case 1: printf("você escolheu depositar\n"); break;
-  case 2: printf("você escolheu sacar\n"); break;
-  case 3: printf("você escoheu consultar\n"); break;
-  case 0: printf("saindo..."); break;
-  default: printf("opção invalida!\n");
-}
+ int numero, soma = 0;
 
+printf("digite numero para somar(0 encerra): 0");
+scanf("%d", &numero);
+
+while(numero != 0){
+  soma += numero;
+  scanf("%d", &numero);
 }
+printf("soma total = %d\n", soma);
+
+
 
 return 0;
-
-
 
 
 }
