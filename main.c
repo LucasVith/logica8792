2909fdeec7ed811c8ab033d681ab8618c934ffb9
LucasVith <lucasvths@gmail.com>
2026-09-23 21:06:28 -0300

    linha

--- main.c
+++ main.c
@@ -2,20 +2,17 @@
 #include<locale.h>
 #include<math.h>
 
-void verificarheparimpar(int n){
-if(n % 2 == 0){
-   printf("%d é par!\n", n);
-}else{
-   printf("%d é ímpar\n", n);
+void linha(int tamanho){
+   for(int i = 0; i < tamanho; i++){
+      printf("*");
+   }
+   printf("\n");
 }
-}
-
 
 int main(){
 setlocale(LC_ALL, "pt_BR.UTF-8");
 
-  verificarheparimpar(10);
-  verificarheparimpar(7);
+  linha(2000);
 
 return 0;
 
