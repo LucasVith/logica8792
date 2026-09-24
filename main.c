eb52a33211a30e01c79c630306723dbb603ff6e3
LucasVith <lucasvths@gmail.com>
2026-09-24 19:21:00 -0300

    while numeros

--- main.c
+++ main.c
@@ -8,13 +8,16 @@ int main(){
 setlocale(LC_ALL, "pt_BR.UTF-8");
 
  
- int i = 1;
-
- while(i <=5){
-  printf("%d\n", i);
-   i++;
+ int numero;
+ printf("digite um número positivo: ");
+ scanf("%d", &numero);
+ 
+ while(numero<=0){
+  printf("número invalido! digite novamente:");
+  scanf("%d", &numero);
  }
 
+printf("número válido: %d\n", numero);
 
 return 0;
 
