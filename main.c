5cac535237306ccc5f5657080111c40d62d6ec1a
LucasVith <lucasvths@gmail.com>
2026-09-24 21:53:53 -0300

    for média de numeros

--- main.c
+++ main.c
@@ -7,24 +7,18 @@
 int main(){
 setlocale(LC_ALL, "pt_BR.UTF-8");
 
-int n;
-int resultado;
+int n; 
+float valor, soma = 0;
 
-printf("digite um numero para a tabuada:" );
-resultado = scanf("%d", &n);
+printf("quantos numeros deseja digitar: ");
+scanf("%d", &n);
 
-if(resultado != 1){
-  printf("entrada invalida! por favor, digite apenas numeros!");
-  return 1;
+for(int i = 0; i <n; i++){
+  printf("digite o numero %d:", i +1);
+  scanf("%f", &valor);
+  soma += valor;
 }
-
-for(int i = 1; i <=10; i ++){
-  int resposta = n * i;
-  printf("o resultado de %d x %d é: %d\n", n, i, resposta);
-}
-
-
-
+printf("média: %.f\n", soma / n);
 
 return 0;
 
