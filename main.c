f9a1a3ab8d95044b730c2d66c5957ec1eaa339a8
LucasVith <lucasvths@gmail.com>
2026-09-25 19:48:30 -0300

    função numero primo

--- main.c
+++ main.c
@@ -2,30 +2,29 @@
 #include<locale.h>
 #include<string.h>
 
+int ehprimo(int n){
+  if(n <2)return 0;
+  for(int i = 2; i < n; i++){
+  if(n % i == 0) return 0;
+  }
+  return 1;
+}
 
 
 int main(){
 setlocale(LC_ALL, "pt_BR.UTF-8");
 
-int n, primo = 1;
-printf("digite um número: ");
-scanf("%d", &n);
+int numero;
 
-if(n<2){
-  primo=0;
-}else{
-  for(int i = 2; i <= n / 2; i++){
-    if(n % i == 0){
-      primo = 0;
-      break;
-    }
-  }
-}
-if(primo){
-  printf("%d é primo\n", n);
-}else{
-  printf("%d não é primo\n",  n);
-}
+printf("digite um número:" );
+scanf("%d", &numero);
+if(ehprimo(numero))
+printf("%d é primo\n", numero);
+else
+printf("%d não é primo\n", numero);
+
+
+
 return 0;
 
 
